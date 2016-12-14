23cfa250536f92a7cb009105fa50099e21310e50
Giuseppe Lo Presti <giuseppe.lopresti@cern.ch>
2016-12-14 15:49:29 +0100

    Added logging infrastructure

--- src/wopiserver.py
+++ src/wopiserver.py
@@ -2,38 +2,62 @@
 #
 # wopiserver.py
 #
-# Initial prototype for an Office Web Apps gateway for CERNBox
+# Initial prototype for a Web-application Open Platform Interface (WOPI) gateway for CERNBox
 #
 # Giuseppe.LoPresti@cern.ch
 
+from XRootD import client as XrdClient
 import flask
-app = flask.Flask(__name__)
+import json
+import logging.handlers
+import logging
+
+app = flask.Flask("WOPI server")
+tokens = {}
+log = logging.FileHandler('wopiserver.log')
+log.setLevel(logging.DEBUG)
+app.logger.addHandler(log)
 
 @app.route("/")
 def index():
-	return "WOPI server top level page"
+  return "This is the WOPI server. Access is performed via REST API, e.g. GET /api/wopi/files/<fileid>"
 
-@app.route("/api/wopi/files/<filename>", methods=['GET'])
-#
-#{
-#  "BaseFileName": "sample string 1",
-#  "OwnerId": "sample string 2",
-#  "Size": 3,
-#  "SHA256": "sample string 4",
-#  "Version": "sample string 5",
-#  "SupportsUpdate": true,
-#  "UserCanWrite": true,
-#  "SupportsLocks": true
-#}
-def wopiGetMetadata(filename):
-    return "GET metadata for %s, access token = %s" % (filename, flask.request.args['access_token'])
+@app.route("/setAccessToken/<fileid>", methods=['GET', 'POST'])   # GET for debugging
+def setAccessToken(fileid):
+  uid = flask.request.args['uid']
+  filename = flask.request.args['filename']
+  tokens[flask.request.args['access_token']] = (fileid, uid, filename)
+  log.info("Access token set for filename %s and user %s" % (filename, uid))
+  return '{}'
 
-@app.route("/api/wopi/files/<filename>/contents", methods=['GET'])
-def wopiGetContent(filename):
-    return "GET content for %s, access token = %s" % (filename, flask.request.args['access_token'])
+@app.route("/api/wopi/files/<fileid>", methods=['GET'])
+def wopiCheckFileInfo(fileid):
+  try:
+    fid, uid, filename = tokens[flask.request.args['access_token']]
+    log.info("GET metadata for %d:%s, access token = %s" % (fileid, filename, flask.request.args['access_token']))
+    md = {}
+    md['Basefileid'] = fileid
+    md['OwnerId'] = uid
+    md['Size'] = 100
+    md['SHA256'] = '1'
+    md['Version'] = '1.0'
+    md['SupportsUpdate'] = md['UserCanWrite'] = md['SupportsLocks'] = true
+    jsonmd = json.dumps(md)
+    return jsonmd
+  except KeyError, e:
+    log.warning("Unknown user/token %s" % flask.request.args['access_token'])
+    log.warning(e)
 
-@app.route("/api/wopi/files/<filename>/contents", methods=['POST'])
-def wopiPostContent(filename):
-    return "POST content for %s" % filename
+@app.route("/api/wopi/files/<fileid>/contents", methods=['GET'])
+def wopiGetFile(fileid):
+  log.info("GET content for %s, access token = %s" % (fileid, flask.request.args['access_token']))
+
+@app.route("/api/wopi/files/<fileid>/contents", methods=['POST'])
+def wopiPostContent(fileid):
+  log.info("NOOP - POST content for %s, access token = %s" % (fileid, flask.request.args['access_token']))
+
+@app.route("/api/Files", methods=['POST'])
+def wopiPostContent():
+  log.info("NOOP - POST content, destinationBlobName = %s" % flask.request.args['destinationBlobName'])
 
 app.run(host='0.0.0.0', port=8080)
