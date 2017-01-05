afef769735548f2390698452e6e152ab537ec15f
Giuseppe Lo Presti <giuseppe.lopresti@cern.ch>
2017-01-05 11:17:23 +0100

    Extracted the xrootd interface code in a separate module

--- src/wopiserver.py
+++ src/wopiserver.py
@@ -10,10 +10,9 @@ import sys, os, time, json, httplib
 import logging.handlers
 import logging
 try:
-  from XRootD import client as XrdClient   # the xroot bindings for python, xrootd-python-4.4.1-1.el7.x86_64.rpm
-  from XRootD.client.flags import OpenFlags
-  import flask                             # Flask app server, python-flask-0.10.1-4.el7.noarch.rpm + pyOpenSSL-0.13.1-3.el7.x86_64.rpm
-  import jwt                               # PyJWT Jason Web Token, python-jwt-1.4.0-2.el7.noarch.rpm
+  import flask                  # Flask app server, python-flask-0.10.1-4.el7.noarch.rpm + pyOpenSSL-0.13.1-3.el7.x86_64.rpm
+  import jwt                    # PyJWT Jason Web Token, python-jwt-1.4.0-2.el7.noarch.rpm
+  from xrootiface import XrdCl  # a wrapper for the xrootd python bindings, xrootd-python-4.4.x.el7.x86_64.rpm
 except:
   print "Missing modules, please install xrootd-python, python-flask, python-jwt"
   sys.exit(-1)
@@ -24,49 +23,7 @@ log = app.logger
 log.setLevel(logging.DEBUG)
 log.addHandler(logging.FileHandler('/var/tmp/wopiserver.log'))    # XXX todo put in a proper place
 wopisecret = 'wopisecret'                          # XXX todo read secret from config file
-# prepare the xroot client
-storageserver = 'root://castorpps'                 # XXX todo read from config file
-homedir = '/castor/cern.ch/user/i/itglp/'
-xrdfs = XrdClient.FileSystem(storageserver)  
-chunksize = 1048576                                # XXX todo read from config file
 tokenvalidity = 86400                              # XXX todo read from config file
-
-# some xrootd useful wrappers
-# XXX make a class/module out of this
-def statXRootFile(filename):
-  rc, statInfo = xrdfs.stat(homedir + filename)
-  if statInfo is None:
-    raise IOError(rc.message)
-  return statInfo
-
-def readXRootFile(filename):
-  with XrdClient.File() as f:
-    rc, _statInfo_unused = f.open(storageserver + '/' + homedir + filename, OpenFlags.READ)
-    if rc.ok == False:
-      # the file could not be opened: as this is a generator, we yield the error string instead of the file's contents
-      log.info('msg="Error opening the file for read" filename="%s" error="%s"' % (filename, rc.message))
-      yield rc.message
-    else:
-      # the actual read is buffered and managed by the Flask server
-      for chunk in f.readchunks(offset=0, chunksize=chunksize):
-        yield chunk
-
-def writeXRootFile(filename, content):
-  f = XrdClient.File()
-  # pass in the URL a special flag to enable the EOS atomic overwrite like the OwnCloud server
-  rc, _statInfo_unused = f.open(storageserver + '/' + homedir + filename, OpenFlags.DELETE)   # overwrite previous version
-  if rc.ok == False:
-    log.info('msg="Error opening the file for write" filename="%s" error="%s"' % (filename, rc.message))
-    raise IOError(rc.message)
-  # XXX write the entire file - we should find a way to only update the required chunks...
-  rc, _statInfo_unused = f.write(content, offset=0)
-  if rc.ok == False:
-    log.info('msg="Error writing the file" filename="%s" error="%s"' % (filename, rc.message))
-    raise IOError(rc.message)
-  rc, _statInfo_unused = f.close()
-  if rc.ok == False:
-    log.info('msg="Error closing the file" filename="%s" error="%s"' % (filename, rc.message))
-    raise IOError(rc.message)
 
 
 # The Web Application starts here
@@ -95,7 +52,7 @@ def wopiCheckFileInfo(fileid):
     if acctok['exp'] < time.time():
       raise jwt.exceptions.DecodeError
     log.info('msg="CheckFileInfo" username="%s" filename"%s" fileid="%s"' % (acctok['username'], acctok['filename'], fileid))
-    statInfo = statXRootFile(acctok['filename'])
+    statInfo = XrdCl(log, acctok['filename']).stat()
     # populate metadata for this file
     md = {}
     md['BaseFileName'] = os.path.basename(acctok['filename'])
@@ -113,6 +70,9 @@ def wopiCheckFileInfo(fileid):
   except IOError, e:
     log.info('msg="Requested file not found" filename="%s" error="%s"' % (acctok['filename'], e))
     return 'File not found', httplib.NOT_FOUND
+  except KeyError, e:
+    log.error('msg="Invalid access token, missing %s field"' % e)
+    return 'Invalid access token', httplib.UNAUTHORIZED
   except Exception, e:
     log.error('msg="Unexpected exception caught" exception="%s"' % e)
     log.debug(sys.exc_info())
@@ -127,7 +87,7 @@ def wopiGetFile(fileid):
       raise jwt.exceptions.DecodeError
     log.info('msg="GetFile" username="%s" filename="%s" fileid="%s"' % (acctok['username'], acctok['filename'], fileid))
     # stream file from storage to client
-    resp = flask.Response(readXRootFile(acctok['filename']), mimetype='application/octet-stream')
+    resp = flask.Response(XrdCl(log, acctok['filename']).readFile(), mimetype='application/octet-stream')
     resp.headers['X-WOPI-ItemVersion'] = '1.0'   # XXX todo get version from server
     return resp
   except jwt.exceptions.DecodeError:
@@ -170,7 +130,7 @@ def wopiPostContent(fileid):
     if acctok['exp'] < time.time():
       raise jwt.exceptions.DecodeError
     log.info('msg="PostContent" username="%s" filename="%s"' % (acctok['username'], acctok['filename']))
-    writeXRootFile(acctok['filename'], flask.request.get_data())
+    XrdCl(log, acctok['filename']).writeFile(flask.request.get_data())
     return 'OK', httplib.OK
   except jwt.exceptions.DecodeError:
     log.warning('msg="Signature verification failed" token="%s"' % flask.request.args['access_token'])
---
+++ src/xrootiface.py
@@ -0,0 +1,59 @@
+#!/bin/python
+#
+# xrootiface.py
+#
+# XRootD interface for the WOPI server for CERNBox
+#
+# Giuseppe.LoPresti@cern.ch
+
+import sys, os, time
+import logging
+from XRootD import client as XrdClient   # the xroot bindings for python, xrootd-python-4.4.x.el7.x86_64.rpm
+from XRootD.client.flags import OpenFlags
+
+class XrdCl:
+
+  def __init__(self, log, filename):
+    self.log = log
+    self.filename = filename
+    # prepare the xroot client
+    self.storageserver = 'root://castorpps'                 # XXX todo read from config file
+    self.homedir = '/castor/cern.ch/user/i/itglp/'
+    self.xrdfs = XrdClient.FileSystem(self.storageserver)  
+    self.chunksize = 1048576                                # XXX todo read from config file
+
+  def stat(self):
+    rc, statInfo = self.xrdfs.stat(self.homedir + self.filename)
+    if statInfo is None:
+      raise IOError(rc.message.strip('\n'))
+    return statInfo
+
+  def readFile(self):
+    with XrdClient.File() as f:
+      rc, _statInfo_unused = f.open(self.storageserver + '/' + self.homedir + self.filename, OpenFlags.READ)
+      if not rc.ok:
+        # the file could not be opened: as this is a generator, we yield the error string instead of the file's contents
+        self.log.info('msg="Error opening the file for read" filename="%s" error="%s"' % (self.filename, rc.message.strip('\n')))
+        yield rc.message
+      else:
+        # the actual read is buffered and managed by the Flask server
+        for chunk in f.readchunks(offset=0, chunksize=self.chunksize):
+          yield chunk
+
+  def writeFile(self, content):
+    f = XrdClient.File()
+    # XXX todo pass in the URL a special flag to enable the EOS atomic overwrite like for the OwnCloud server
+    rc, _statInfo_unused = f.open(self.storageserver + '/' + self.homedir + self.filename, OpenFlags.DELETE)
+    if not rc.ok:
+      self.log.info('msg="Error opening the file for write" filename="%s" error="%s"' % (self.filename, rc.message.strip('\n')))
+      raise IOError(rc.message.strip('\n'))
+    # write the file. In a future implementation, we should find a way to only update the required chunks...
+    rc, _statInfo_unused = f.write(content, offset=0)
+    if not rc.ok:
+      self.log.info('msg="Error writing the file" filename="%s" error="%s"' % (self.filename, rc.message.strip('\n')))
+      raise IOError(rc.message.strip('\n'))
+    rc, _statInfo_unused = f.close()
+    if not rc.ok:
+      self.log.info('msg="Error closing the file" filename="%s" error="%s"' % (self.filename, rc.message.strip('\n')))
+      raise IOError(rc.message.strip('\n'))
+
