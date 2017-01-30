885766472a1a8cc05bfeec7f8e5812d4025af903
Giuseppe Lo Presti <giuseppe.lopresti@cern.ch>
2017-01-30 18:59:38 +0100

    First implementation of the WOPI locking interface

--- src/wopiserver.py
+++ src/wopiserver.py
@@ -24,7 +24,8 @@ WOPISERVERVERSION = 'git'
 
 # these are the xattr keys used for conflicts resolution and locking on the remote storage
 kLastWopiSaveTime = 'oc.wopi.lastwritetime'
-kWopiLock = 'oc.wopi.lock'
+kWopiLockValue = 'oc.wopi.lockvalue'
+kWopiLockExp = 'oc.wopi.lockexpiration'
 
 try:
   _loglevels = {"Critical": logging.CRITICAL,  # 50
@@ -106,7 +107,7 @@ def index():
 
 @app.route("/wopi/cboxopen", methods=['GET'])
 def wopiOpen():
-  '''Get a WOPISrc target and an access token to be passed to Microsoft Office online for accessing a given file for a given user'''
+  '''Return a WOPISrc target and an access token to be passed to Microsoft Office online for accessing a given file for a given user'''
   refreshConfig()
   req = flask.request
   # first resolve the client: only our OwnCloud servers shall use this API
@@ -116,13 +117,13 @@ def wopiOpen():
       for ip in socket.getaddrinfo(c, None):
         if ip[4][0] == req.remote_addr:
           # we got a match, generate the access token
-          ruid = req.args['ruid']
-          rgid = req.args['rgid']
+          ruid = int(req.args['ruid'])
+          rgid = int(req.args['rgid'])
           filename = req.args['filename']
           canedit = ('canedit' in req.args and req.args['canedit'].lower() == 'yes')
           try:
-            inode, acctok = generateAccessToken(ruid, rgid, filename, canedit)
-            log.info('msg="wopiOpen: access token set" host="%s" user="%s:%s" filename="%s" canedit="%r" inode="%s" token="%s"' % \
+            inode, acctok = generateAccessToken(str(ruid), str(rgid), filename, canedit)
+            log.info('msg="wopiOpen: access token set" host="%s" user="%d:%d" filename="%s" canedit="%r" inode="%s" token="%s"' % \
                      (req.remote_addr, ruid, rgid, filename, canedit, inode, acctok))
             # return an URL-encoded URL for the Office Online server
             return urllib.quote_plus('http://%s:8080/wopi/files/%s' % (socket.gethostname(), inode)) + \
@@ -131,7 +132,6 @@ def wopiOpen():
             return 'File not found', httplib.NOT_FOUND
     except socket.gaierror:
       log.warning('msg="wopiOpen: %s found in configured allowed clients but unknown by DNS resolution"' % c)
-      pass
   # no match found, fail
   log.info('msg="wopiOpen: unauthorized access attempt" client="%s"' % flask.request.remote_addr)
   return 'Client IP not authorized', httplib.UNAUTHORIZED
@@ -201,32 +201,62 @@ def wopiGetFile(fileid):
     log.debug(sys.exc_info())
     return 'Internal error', httplib.INTERNAL_SERVER_ERROR
 
-
+#
 # the following operations are all called on POST /wopi/files/<fileid>
-
+#
 def wopiLock(fileid, reqheaders, acctok):
   '''Implements the Lock and RefreshLock WOPI calls'''
   lock = reqheaders['X-WOPI-Lock']
-  log.info('msg="%s" user="%s:%s" filename="%s" fileid="%s" lock="%s"' % \
-           (reqheaders['X-WOPI-Override'].title(), acctok['ruid'], acctok['rgid'], acctok['filename'], fileid, lock))
-  if reqheaders['X-WOPI-Override'] == 'LOCK':
-    # on first lock, set an xattr with the current time for later conflicts checking
-    try:
-      xrdcl.setxattr(acctok['filename'], acctok['ruid'], acctok['rgid'], kLastWopiSaveTime, int(time.time()))
-    except IOError:
-      # not fatal, but will generate a conflict file later on, so log a warning
-      log.warning('msg="Unable to set xattr" user="%s:%s" filename="%s" fileid="%s"' % \
-                  (acctok['ruid'], acctok['rgid'], acctok['filename'], fileid))
-  return 'OK', httplib.OK
+  oldLock = reqheaders['X-WOPI-OldLock'] if 'X-WOPI-OldLock' in reqheaders else ''
+  op = reqheaders['X-WOPI-Override']
+  try:
+    retrievedLock = xrdcl.getxattr(acctok['filename'], acctok['ruid'], acctok['rgid'], kWopiLockValue)
+    lockExpiration = int(xrdcl.getxattr(acctok['filename'], acctok['ruid'], acctok['rgid'], kWopiLockExp))
+  except IOError:
+    retrievedLock = ''     # no pre-existing lock found
+    lockExpiration = 0
+  log.info('msg="%s" user="%s:%s" filename="%s" fileid="%s" lock="%s" oldLock="%s" retrievedLock="%s" expTime="%s"' % \
+           (op.title(), acctok['ruid'], acctok['rgid'], acctok['filename'], fileid, lock, oldLock, retrievedLock, \
+            time.strftime('%Y-%m-%dT%H:%M:%S', time.localtime(lockExpiration))))
+  if lockExpiration < time.time():
+    # the retrieved lock is not valid any longer
+    retrievedLock = ''
+  # perform the required checks for the validity of the new lock
+  if (oldLock == '' and retrievedLock != '' and retrievedLock != lock) or (oldLock != '' and retrievedLock != oldLock):
+    resp = flask.Response()
+    resp.headers['X-WOPI-Lock'] = retrievedLock
+    log.info('msg="%s" filename="%s" lock="%s" retrievedLock="%s" result="conflict"' % \
+             (op.title(), acctok['filename'], lock, retrievedLock))
+    resp.status_code = httplib.CONFLICT
+    return resp
+  if op == 'REFRESH_LOCK' and lockExpiration < time.time():
+    log.info('msg="%s" filename="%s" lock="%s" result="expired"' % (op.title(), acctok['filename'], lock))
+    return 'Lock %s has expired' % lock, httplib.BAD_REQUEST
+  else:  # LOCK or REFRESH_LOCK: set the lock to the given one, and record its expiration time
+    xrdcl.setxattr(acctok['filename'], acctok['ruid'], acctok['rgid'], kWopiLockValue, lock)
+    xrdcl.setxattr(acctok['filename'], acctok['ruid'], acctok['rgid'], kWopiLockExp, \
+                   time.time() + config.getint('general', 'wopilockexpiration'))
+    log.info('msg="%s" filename="%s" lock="%s" result="success"' % (op.title(), acctok['filename'], lock))
+    if retrievedLock == '':
+      # on first lock, set an xattr with the current time for later conflicts checking
+      try:
+        xrdcl.setxattr(acctok['filename'], acctok['ruid'], acctok['rgid'], kLastWopiSaveTime, int(time.time()))
+      except IOError:
+        # not fatal, but will generate a conflict file later on, so log a warning
+        log.warning('msg="Unable to set xattr" user="%s:%s" filename="%s" fileid="%s"' % \
+                    (acctok['ruid'], acctok['rgid'], acctok['filename'], fileid))
+    return 'OK', httplib.OK
 
 def wopiUnlock(fileid, reqheaders, acctok):
   '''Implements the Unlock WOPI call'''
   lock = reqheaders['X-WOPI-Lock']
   log.info('msg="Unlock" user="%s:%s" filename="%s" fileid="%s" lock="%s"' % \
            (acctok['ruid'], acctok['rgid'], acctok['filename'], fileid, lock))
-  # remove any extended attribute related to conflicts handling
+  # remove any extended attribute related to locks and conflicts handling
   try:
     xrdcl.rmxattr(acctok['filename'], acctok['ruid'], acctok['rgid'], kLastWopiSaveTime)
+    xrdcl.rmxattr(acctok['filename'], acctok['ruid'], acctok['rgid'], kWopiLockValue)
+    xrdcl.rmxattr(acctok['filename'], acctok['ruid'], acctok['rgid'], kWopiLockExp)
   except IOError:
     # ignore, it's not worth to report anything here
     pass
@@ -235,7 +265,17 @@ def wopiUnlock(fileid, reqheaders, acctok):
 def wopiGetLock(fileid, reqheaders_unused, acctok):
   '''Implements the GetLock WOPI call'''
   log.info('msg="GetLock" user="%s:%s" filename="%s" fileid="%s"' % (acctok['ruid'], acctok['rgid'], acctok['filename'], fileid))
-  return 'Not supported', httplib.NOT_IMPLEMENTED
+  resp = flask.Response()
+  try:
+    resp.headers['X-WOPI-Lock'] = xrdcl.getxattr(acctok['filename'], acctok['ruid'], acctok['rgid'], kWopiLockValue)
+    lockExpiration = int(xrdcl.getxattr(acctok['filename'], acctok['ruid'], acctok['rgid'], kWopiLockExp))
+    if lockExpiration < time.time():
+      resp.headers['X-WOPI-Lock'] = ''    # the lock is not valid any longer, discard
+  except (IOError, ValueError) as e_unused:
+    # nothing to return
+    resp.headers['X-WOPI-Lock'] = ''
+  resp.status_code = httplib.OK
+  return resp
 
 def wopiPutRelative(fileid, reqheaders_unused, acctok):
   '''Implements the PutRelative WOPI call'''
@@ -259,7 +299,7 @@ def wopiRenameFile(fileid, reqheaders_unused, acctok):
   return 'Not supported', httplib.NOT_IMPLEMENTED
 
 @app.route("/wopi/files/<fileid>", methods=['POST'])
-def wopiPost(fileid):
+def wopiFilesPost(fileid):
   '''A dispatcher metod for all files POST operations'''
   refreshConfig()
   try:
@@ -332,7 +372,7 @@ def wopiPutFile(fileid):
     # Anyhow, previous versions are all stored and recoverable by the user.
     xrdcl.writefile(acctok['filename'], acctok['ruid'], acctok['rgid'], flask.request.get_data())
     log.info('msg="File successfully written" user="%s:%s" filename="%s"' % (acctok['ruid'], acctok['rgid'], acctok['filename']))
-    # and retrieve again the modification time to update our xattr
+    # and set again our xattr
     xrdcl.setxattr(acctok['filename'], acctok['ruid'], acctok['rgid'], kLastWopiSaveTime, int(time.time()))
     return 'OK', httplib.OK
   except (jwt.exceptions.DecodeError, jwt.exceptions.ExpiredSignatureError) as e:
@@ -349,4 +389,4 @@ def wopiPutFile(fileid):
 
 # start the Flask endless listening loop
 app.run(host='0.0.0.0', port=8080, threaded=True, debug=(config.get('general', 'loglevel') == 'Debug'))
-# XXX todo: enable https and then add:    ssl_context=(config.get('security', 'wopicert'), config.get('security', 'wopikey')))
+# XXX todo: enable https on port 443 and then add:    ssl_context=(config.get('security', 'wopicert'), config.get('security', 'wopikey')))
