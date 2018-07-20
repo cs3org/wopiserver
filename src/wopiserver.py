eb1f7d02685fdc9339c98feb1d1277a2e2e4e70b
Giuseppe Lo Presti <giuseppe.lopresti@cern.ch>
2018-07-20 16:07:56 +0200

    Added the OfficeVersion given by Office Online in the initial log

--- src/wopiserver.py
+++ src/wopiserver.py
@@ -430,8 +430,9 @@ def wopiCheckFileInfo(fileid):
     acctok = jwt.decode(flask.request.args['access_token'], Wopi.wopisecret, algorithms=['HS256'])
     if acctok['exp'] < time.time():
       raise jwt.exceptions.ExpiredSignatureError
-    Wopi.log.info('msg="CheckFileInfo" user="%s:%s" filename="%s" fileid="%s" token="%s"' % \
-                  (acctok['ruid'], acctok['rgid'], acctok['filename'], fileid, flask.request.args['access_token'][-20:]))
+    Wopi.log.info('msg="CheckFileInfo" user="%s:%s" filename="%s" fileid="%s" token="%s" officeversion="%s"' % \
+                  (acctok['ruid'], acctok['rgid'], acctok['filename'], fileid, flask.request.args['access_token'][-20:], \
+                   flask.request.headers['X-OfficeVersion'] if 'X-OfficeVersion' in flask.request.headers else 'Unknown'))
     statInfo = xrdcl.statx(acctok['endpoint'], acctok['filename'], acctok['ruid'], acctok['rgid'])
     # compute some entities for the response
     wopiSrc = 'WOPISrc=%s&access_token=%s' % \
