95d9f34a1d56695137a5008bd0c6fcc3519af69c
Giuseppe Lo Presti <giuseppe.lopresti@cern.ch>
2020-03-11 15:26:25 +0100

    Make sure the dowloadurl option is configured

--- src/wopiserver.py
+++ src/wopiserver.py
@@ -99,6 +99,7 @@ class Wopi:
         cls.lockpath = cls.config.get('general', 'lockpath')
       else:
         cls.lockpath = ''
+      _ = cls.config.get('general', 'downloadurl')   # make sure this is defined
     except Exception as e:
       # any error we get here with the configuration is fatal
       cls.log.fatal('msg="Failed to initialize the service, aborting" error="%s"' % e)
