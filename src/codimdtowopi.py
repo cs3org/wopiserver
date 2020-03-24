e40fec0df45e5952286cbf3b52f2743d438dbe94
Giuseppe Lo Presti <giuseppe.lopresti@cern.ch>
2020-03-24 12:30:26 +0100

    First PoC of a bridge to connect a CodiMD server to WOPI

---
+++ src/codimdtowopi.py
@@ -0,0 +1,206 @@
+#!/usr/bin/python3
+'''
+codimdtowopi.py
+
+The CodiMD to WOPI gateway for CERNBox
+
+Author: Giuseppe.LoPresti@cern.ch, CERN/IT-ST
+'''
+
+import sys
+import time
+import traceback
+import socket
+import configparser
+from platform import python_version
+import logging
+import logging.handlers
+import urllib.parse
+import http.client
+import requests
+try:
+  import flask                   # Flask app server
+except ImportError:
+  print("Missing modules, please install Flask with `pip3 install flask`")
+  raise
+
+MDWSERVERVERSION = '0.1'
+
+class MDW:
+  '''A singleton container for all state information of the server'''
+  app = flask.Flask("CodiMDToWOPI")
+  port = 0
+  lastConfigReadTime = time.time()
+  loglevels = {"Critical": logging.CRITICAL,  # 50
+               "Error":    logging.ERROR,     # 40
+               "Warning":  logging.WARNING,   # 30
+               "Info":     logging.INFO,      # 20
+               "Debug":    logging.DEBUG      # 10
+              }
+  log = app.logger
+  openDocs = {}
+  frame_page_templated_html = """
+    <html>
+    <head>
+    <title>WOPI-integrated CodiMD PoC</title>
+    <style type="text/css">
+      body, html
+      {
+        margin: 0; padding: 0; height: 100%%; overflow: hidden;
+      }
+    </style>
+    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <script>
+      window.onbeforeunload = function() {
+        $.get("http://%s:%d/close",     // TODO handle https
+          {access_token: '%s'},
+          function(data) {
+            alert(data)
+          }
+        );
+      };
+      $(document).ready(function() {
+        $('a[rel!=ext]').click(function() { window.onbeforeunload = null; });
+        $('form').submit(function() { window.onbeforeunload = null; });
+      });
+    </script>
+    </head>
+    <body>
+    <iframe width="100%%" height="100%%" src="%s"></iframe>
+    </body>
+    </html>
+    """
+
+  @classmethod
+  def init(cls):
+    '''Initialises the application, bails out in case of failures. Note this is not a __init__ method'''
+    try:
+      # configure the logging
+      loghandler = logging.FileHandler('/var/log/wopi/codimdtowopi.log')
+      loghandler.setFormatter(logging.Formatter(fmt='%(asctime)s %(name)s[%(process)d] %(levelname)-8s %(message)s',
+                                                datefmt='%Y-%m-%dT%H:%M:%S'))
+      cls.log.addHandler(loghandler)
+      # read the configuration
+      cls.config = configparser.ConfigParser()
+      cls.config.read_file(open('/etc/wopi/codimdtowopi.defaults.conf'))
+      cls.config.read('/etc/wopi/codimdtowopi.conf')
+      # prepare the Flask web app
+      cls.port = int(cls.config.get('general', 'port'))
+      cls.log.setLevel(cls.loglevels[cls.config.get('general', 'loglevel')])
+      cls.codimdurl = cls.config.get('general', 'codimdurl')
+      cls.useHttps = False
+    except Exception as e:
+      # any error we get here with the configuration is fatal
+      cls.log.fatal('msg="Failed to initialize the service, aborting" error="%s"' % e)
+      sys.exit(-22)
+
+
+  @classmethod
+  def run(cls):
+    '''Runs the Flask app in either secure (https) or test (http) mode'''
+    if cls.useHttps:
+      cls.log.info('msg="CodiMD to WOPI Server starting in secure mode" port="%d"' % cls.port)
+      cls.app.run(host='0.0.0.0', port=cls.port, threaded=True, debug=(cls.config.get('general', 'loglevel') == 'Debug'),
+                  ssl_context=(cls.config.get('security', 'wopicert'), cls.config.get('security', 'wopikey')))
+    else:
+      cls.log.info('msg="CodiMD to WOPI Server starting in test/unsecure mode" port="%d"' % cls.port)
+      cls.app.run(host='0.0.0.0', port=cls.port, threaded=True, debug=(cls.config.get('general', 'loglevel') == 'Debug'))
+
+
+#############################################################################################################
+#
+# The Web Application starts here
+#
+#############################################################################################################
+
+@MDW.app.route("/", methods=['GET'])
+def index():
+  '''Return a default index page with some user-friendly information about this service'''
+  MDW.log.info('msg="Accessed index page" client="%s"' % flask.request.remote_addr)
+  return """
+    <html><head><title>CodiMD to WOPI</title></head>
+    <body>
+    <div align="center" style="color:#000080; padding-top:50px; font-family:Verdana; size:11">
+    This is the CodiMD to WOPI bridge, to be used in conjunction with a WOPI-enabled EFSS.</div>
+    <br><br><br><br><br><br><br><hr>
+    <i>CERNBox CodiMD to WOPI Server %s at %s. Powered by Flask %s for Python %s</i>.
+    </body>
+    </html>
+    """ % (MDWSERVERVERSION, socket.getfqdn(), flask.__version__, python_version())
+
+
+@MDW.app.route("/open", methods=['GET'])
+def mdOpen():
+  '''Open a md doc by contacting the provided WOPISrc with the given access_token'''
+  wopiSrc = urllib.parse.unquote(flask.request.args['WOPISrc'])
+  acctok = flask.request.args['access_token']
+  MDW.log.info('msg="open called" client="%s" token="%s"' % (flask.request.remote_addr, acctok[-20:]))
+
+  # TODO WOPI GetFileInfo
+  #openDocs[acctok] = requests.get(wopiSrc)
+
+  # WOPI GetFile
+  url = '%s/contents?access_token=%s' % (wopiSrc, acctok)
+  MDW.log.debug('msg="Calling WOPI" url="%s"' % url)
+  res = requests.get(url)
+  if res.status_code != http.client.OK:
+    raise ValueError(res.status_code)
+  mddoc = res.content
+
+  # WOPI Lock
+  url = '%s?access_token=%s' % (wopiSrc, acctok)
+  MDW.log.debug('msg="Calling WOPI" url="%s"' % url)
+  res = requests.post(url, headers={'X-WOPI-Lock': 'TODO hash', 'X-Wopi-Override': 'LOCK'})
+  if res.status_code != http.client.OK:
+    # TODO handle conflicts
+    raise ValueError(res.status_code)
+
+  # now push the document to CodiMD
+  res = requests.post(MDW.codimdurl + '/new', data=mddoc, allow_redirects=False, \
+                      headers={'Content-Type': 'text/markdown'})
+  if res.status_code != http.client.FOUND:
+    raise ValueError(res.status_code)
+  MDW.openDocs[acctok] = { 'codimd': res.next.url, 'wopiSrc': wopiSrc }   # this is the redirect with the hash of the document just created
+  MDW.log.info('msg="Pushed document to CodiMD" url="%s" token="%s"' % (MDW.openDocs[acctok]['codimd'], acctok[-20:]))
+  
+  # generate a hook for close and return an iframe to the client
+  return MDW.frame_page_templated_html % (socket.getfqdn(), MDW.port, acctok, MDW.openDocs[acctok]['codimd'])
+
+
+@MDW.app.route("/close", methods=['GET'])
+def mdClose():
+  acctok = flask.request.args['access_token']
+  wopiSrc = MDW.openDocs[acctok]['wopiSrc']   # TODO handle missing key
+
+  # Get document from CodiMD
+  res = requests.get(MDW.openDocs[acctok]['codimd'] + '/download')
+  if res.status_code != http.client.OK:
+    raise ValueError(res.status_code)
+  mddoc = res.content
+
+  # WOPI PutFile
+  url = '%s/contents?access_token=%s' % (wopiSrc, acctok)
+  MDW.log.debug('msg="Calling WOPI" url="%s"' % url)
+  res = requests.post(url, headers={'X-WOPI-Lock': 'TODO hash'}, data=mddoc)
+  if res.status_code != http.client.OK:
+    # TODO handle conflicts
+    raise ValueError(res.status_code)
+
+  # WOPI Unlock
+  url = '%s?access_token=%s' % (wopiSrc, acctok)
+  MDW.log.debug('msg="Calling WOPI" url="%s"' % url)
+  res = requests.post(url, headers={'X-WOPI-Lock': 'TODO hash', 'X-Wopi-Override': 'UNLOCK'})
+  if res.status_code != http.client.OK:
+    # TODO handle conflicts
+    raise ValueError(res.status_code)
+
+  # TODO delete on codimd
+  MDW.log.info('msg="close called" client="%s" token="%s"' % (flask.request.remote_addr, acctok[-20:]))
+  return 'OK', http.client.OK
+
+#
+# Start the Flask endless listening loop if started in standalone mode
+#
+if __name__ == '__main__':
+  MDW.init()
+  MDW.run()
