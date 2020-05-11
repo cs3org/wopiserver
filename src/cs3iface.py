28448b25f5e7ffe64f374c58cc4a1c940ccc12a8
Lovisa Lugnegard <lovisa.lugnegard@gmail.com>
2020-05-11 08:26:54 +0200

    Update file references

--- src/cs3iface.py
+++ src/cs3iface.py
@@ -12,6 +12,8 @@ import sys
 import time
 import os
 import grpc
+import requests 
+from tusclient import client as tusclient
 
 # TODO package python-cs3apis bindings. For now, we include them like this
 # insert at 1, 0 is the script path (or '' in REPL)
@@ -121,75 +123,92 @@ def rmxattr(_endpoint, filename, key):
         raise IOError(e)
 
 
-def readfile(_endpoint, filename, _ruid, _rgid):
-    '''Read a file on behalf of the given uid, gid. Note that the function is a generator, managed by Flask.'''
-    print('msg="Invoking readFile" filename="%s"' % filename)
+def readfile(_endpoint, filepath, _ruid, _rgid):
+    '''Read a file on behalf of the given filepath. Note that the function is a generator, managed by Flask.'''
+    print('msg="Invoking readFile" filename="%s"' % filepath)
     try:
-        tstart = time.clock()
-        # chunksize = config.getint('io', 'chunksize')
-        reference = spr.Reference(path = 'example.txt', id = spr.ResourceId(storage_id = '123e4567-e89b-12d3-a456-426655440000', opaque_id = 'fileid-home/example.txt'))
+        chunksize = 2 # config.getint('io', 'chunksize')
+        reference = spr.Reference(path = filepath)
         req = sp.InitiateFileDownloadRequest(ref = reference)
-        res = credentials['cs3stub'].InitiateFileDownload(request = req, metadata = [('x-access-token', credentials['token'])])
-        #TODO open and actually read file
-        tend = time.clock()
-        print('res' + res)
-        print('msg="File open for read" filename="%s" elapsedTimems="%.1f"' % (
-            filename, (tend-tstart)*1000))
-    except FileNotFoundError as e:
-        print('msg="File not found on read" filename="%s"' % filename)
+        initiatefiledownloadres = credentials['cs3stub'].InitiateFileDownload(request = req, metadata = [('x-access-token', credentials['token'])])
+
+        #Download
+        url = initiatefiledownloadres.download_endpoint
+        headers = {'X-Reva-Transfer': initiatefiledownloadres.token, 'Authorization': 'Basic ZWluc3RlaW46cmVsYXRpdml0eQ=='}
+        fileinformation = requests.get(url=url, headers=headers)
+
+        data = fileinformation.content
+        for i in range(0, len(data), chunksize):
+          yield data[i:i+chunksize]
+    except Exception as e:
+      print('msg="Error when reading file" filepath="%s" error="%s"' % (filepath))
+      raise IOError(e)
 
 
-def writefile(_endpoint, filename, content, noversion=0):
+def writefile(_endpoint, filepath, content, noversion=0):
     '''Write a file via cs3 apis on behalf of the given uid, gid. The entire content is written
        and any pre-existing file is deleted (or moved to the previous version if supported).
        If noversion=1, the write explicitly disables versioning: this is useful for lock files.'''
-    size = len(content)
-    print('msg="Invoking writeFile" filename="%s" size="%d"' %
-              (filename, size))
+   # size = len(content)
+    # print('msg="Invoking writeFile" filename="%s" size="%d"' %
+    #           (filepath, size))
     try:
         tstart = time.clock()
         # get file to write to, do we need this?
-        reference = spr.Reference(path = 'example.txt', id = spr.ResourceId(storage_id = '123e4567-e89b-12d3-a456-426655440000', opaque_id = 'fileid-home/example.txt'))
-        req = sp.InitiateFileDownloadRequest(ref = reference)
-        res = credentials['cs3stub'].InitiateFileDownload(request = req, metadata = [('x-access-token', credentials['token'])])
-        f = open(filename, mode='wb')
+        # reference = spr.Reference(path = filepath)
+        # req = sp.InitiateFileDownloadRequest(ref = reference)
+        # res = credentials['cs3stub'].InitiateFileDownload(request = req, metadata = [('x-access-token', credentials['token'])])
+        # url = initiatefiledownloadres.download_endpoint
+        # headers = {'X-Reva-Transfer': initiatefiledownloadres.token, 'Authorization': 'Basic ZWluc3RlaW46cmVsYXRpdml0eQ=='}
+        # fileinformation = requests.get(url=url, headers=headers)
+        #f = open(filepath, mode='wb')
         tend = time.clock()
-        log.info('msg="File open for write" filename="%s" elapsedTimems="%.1f"' % (filename, (tend-tstart)*1000))
+        print('msg="File open for write" filename="%s" elapsedTimems="%.1f"' % (filepath, (tend-tstart)*1000))
         # write the file. In a future implementation, we should find a way to only update the required chunks...
-        written = f.write(content)
-        f.close()
+        # written = f.write(content)
+        # f.close()
         tstart = time.clock()
-        reference = spr.Reference(path = 'example.txt', id = spr.ResourceId(storage_id = '123e4567-e89b-12d3-a456-426655440000', opaque_id = 'fileid-home/example.txt'))
+        reference = spr.Reference(path = filepath)
         req = sp.InitiateFileUploadRequest(ref = reference)
-        res = credentials['cs3stub'].InitiateFileUpload(request = req, metadata = [('x-access-token', credentials['token'])])
+        res1 = credentials['cs3stub'].InitiateFileUpload(request = req, metadata = [('x-access-token', credentials['token'])])
+        headers = {'X-Reva-Transfer': res1.token, 'Authorization': 'Basic ZWluc3RlaW46cmVsYXRpdml0eQ=='}
+        my_client = tusclient.TusClient(res1.upload_endpoint, headers=headers)
+
+        metadata = {
+          "filename": filepath,
+          "dir":      "home"
+        }
+
+        uploader = my_client.uploader(file_path='src/example.txt', chunk_size=200, metadata = metadata)
+        print("res1.text" + res1.upload_endpoint)
+        uploader.upload_chunk()
         tend = time.clock()
         print('msg="File open for write" filename="%s" elapsedTimems="%.1f"' % (
-            filename, (tend-tstart)*1000))
-        if res.status.code != 1:
-            raise IOError('Something went wrong, message: ' + res.status.message)
+            filepath, (tend-tstart)*1000))
+        # if res.status.code != 1:
+        #     raise IOError('Something went wrong, message: ' + res.status.message)
     except OSError as e:
-        print(
-            'msg="Error writing to file" filename="%s" error="%s"' % (filename, e))
+        print('msg="Error writing to file" filename="%s" error="%s"' % (filepath, e))
         raise IOError(e)
 
 
-def renamefile(_endpoint, origfilename, newfilename, ruid, rgid):
+def renamefile(_endpoint, filepath, newfilepath, ruid, rgid):
     '''Rename a file from origfilename to newfilename on behalf of the given uid, gid.'''
-    source = spr.Reference(path = 'example.txt', id = spr.ResourceId(storage_id = '123e4567-e89b-12d3-a456-426655440000', opaque_id = 'fileid-home/example.txt'))
-    destination = spr.Reference(path = newfilename, id = spr.ResourceId(storage_id = '123e4567-e89b-12d3-a456-426655440000', opaque_id = 'fileid-home/' +  newfilename))
+    source = spr.Reference(path = filepath)
+    destination = spr.Reference(path = newfilepath)
     req = sp.MoveRequest(source = source, destination = destination)
     try:
         credentials['cs3stub'].Move(request = req, metadata = [('x-access-token', credentials['token'])])
-    except (FileNotFoundError, PermissionError) as e:
+    except Exception as e:
         raise IOError(e)
 
 
-def removefile(_endpoint, filename, _ruid, _rgid, _force=0):
+def removefile(_endpoint, filepath, _ruid, _rgid, _force=0):
     '''Remove a file on behalf of the given uid, gid.
        The force argument is irrelevant and ignored for local storage.'''
-    reference = spr.Reference(path = 'example.txt', id = spr.ResourceId(storage_id = '123e4567-e89b-12d3-a456-426655440000', opaque_id = 'fileid-home/example.txt'))
+    reference = spr.Reference(path = filepath)
     req = sp.DeleteRequest(ref = reference)
     try:
         credentials['cs3stub'].Delete(request = req, metadata = [('x-access-token', credentials['token'])])
-    except (FileNotFoundError, PermissionError, IsADirectoryError) as e:
+    except Exception as e:
         raise IOError(e)
