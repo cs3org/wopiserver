7237372aa05f8fc6e2baf01477ab47a813e43209
Giuseppe Lo Presti <giuseppe.lopresti@cern.ch>
2021-05-19 11:36:59 +0200

    Use proper padID for import and export, now they work

--- poc_src/etherpad.py
+++ poc_src/etherpad.py
@@ -55,24 +55,25 @@ def jsonify(msg):
     return '{"message": "%s"}' % msg
 
 
-def _padid(padname):
-    '''One-liner to generate a padID from a padName'''
-    return groupid + '$' + padname
-
-
 def _apicall(method, params, data=None, acctok=None, raiseonnonzerocode=True):
     '''Generic method to call the Etherpad REST API'''
     params['apikey'] = apikey
-    res = requests.post(appurl + '/api/1/' + method, params=params, data=data, verify=not skipsslverify)
-    if res.status_code != http.client.OK:
-        log.error('msg="Failed to call Etherpad" method="%s" token="%s" response="%d: %s"' %
-                    (method, acctok[-20:] if acctok else 'N/A', res.status_code, res.content.decode()))
+    try:
+        res = requests.post(appurl + '/api/1/' + method, params=params, data=data, verify=not skipsslverify)
+        if res.status_code != http.client.OK:
+            log.error('msg="Failed to call Etherpad" method="%s" token="%s" response="%d: %s"' %
+                        (method, acctok[-20:] if acctok else 'N/A', res.status_code, res.content.decode()))
+            raise AppFailure
+    except requests.exceptions.ConnectionError as e:
+        log.error('msg="Exception raised attempting to connect to CodiMD" exception="%s"' % e)
         raise AppFailure
     res = res.json()
     if res['code'] != 0 and raiseonnonzerocode:
         log.error('msg="Error response from Etherpad" method="%s" token="%s" response="%s"' %
                     (method, acctok[-20:] if acctok else 'N/A', res['message']))
         raise AppFailure
+    log.debug('msg="Called Etherpad API" method="%s" token="%s" result="%s"' %
+              (method, acctok[-20:] if acctok else 'N/A', res))
     return res
 
 
@@ -96,22 +97,6 @@ def getredirecturl(isreadwrite, wopisrc, acctok, wopilock, displayname):
 # Cloud storage to Etherpad
 ###########################
 
-def _fetchfrometherpad(wopilock, acctok):
-    '''Fetch a given document from from Etherpad, raise AppFailure in case of errors'''
-    try:
-        # this request does not use the API, so we strip the `/api/1` part
-        res = requests.get(appurl + '/p/' + _padid(wopilock['docid'][1:]) + '/export/etherpad',
-                           verify=not skipsslverify)
-        if res.status_code != http.client.OK:
-            log.error('msg="Unable to fetch document from Etherpad" token="%s" response="%d: %s"' %
-                      (acctok[-20:], res.status_code, res.content.decode()))
-            raise AppFailure
-        return res.content
-    except requests.exceptions.ConnectionError as e:
-        log.error('msg="Exception raised attempting to connect to Etherpad" exception="%s"' % e)
-        raise AppFailure
-
-
 def loadfromstorage(filemd, wopisrc, acctok, docid):
     '''Copy document from storage to Etherpad'''
     # WOPI GetFile
@@ -126,17 +111,20 @@ def loadfromstorage(filemd, wopisrc, acctok, docid):
         if not docid:
             docid = ''.join([choice(ascii_lowercase) for _ in range(20)])
             log.debug('msg="Generated random padID for read-only document" docid="%s" token="%s"' % (docid, acctok[-20:]))
-        # create pad with the given docid as name (the padID will be `groupid$docid`)
+        # first drop previous pad if it exists
+        res = _apicall('deletePad', {'padID': docid}, acctok=acctok, raiseonnonzerocode=False)
+        # create pad with the given docid as name
         res = _apicall('createGroupPad', {'groupID': groupid, 'padName': docid, 'text': 'placeholder'},
-                        acctok=acctok, raiseonnonzerocode=False)
+                       acctok=acctok, raiseonnonzerocode=False)
         log.debug('msg="Got pad" token="%s" docid="%s" response="%s"' % (acctok[-20:], docid, res))
         # push content
-        res = requests.post(appurl + '/p/' + _padid(docid) + '/import',
+        res = requests.post(appurl + '/p/' + docid + '/import',
                             files={'file': (docid + '.etherpad', epfile)},    # a .etherpad file is imported as raw (JSON) content
+                            params={'apikey': apikey},
                             verify=not skipsslverify)
         if res.status_code != http.client.OK:
             log.error('msg="Unable to push document to Etherpad" token="%s" response="%d: %s"' %
-                        (acctok[-20:], res.status_code, res.content.decode()))
+                      (acctok[-20:], res.status_code, res.content.decode()))
             raise AppFailure
         log.info('msg="Pushed document to Etherpad" docid="%s" token="%s"' % (docid, acctok[-20:]))
     except requests.exceptions.ConnectionError as e:
@@ -149,12 +137,28 @@ def loadfromstorage(filemd, wopisrc, acctok, docid):
 # Etherpad to cloud storage
 ###########################
 
+def _fetchfrometherpad(wopilock, acctok):
+    '''Fetch a given document from from Etherpad, raise AppFailure in case of errors'''
+    try:
+        # this operation does not use the API (and it is NOT protected by the API key!), so we use a plain GET
+        res = requests.get(appurl + '/p' + wopilock['docid'] + '/export/etherpad',
+                           verify=not skipsslverify)
+        if res.status_code != http.client.OK:
+            log.error('msg="Unable to fetch document from Etherpad" token="%s" response="%d: %s"' %
+                      (acctok[-20:], res.status_code, res.content.decode()))
+            raise AppFailure
+        return res.content
+    except requests.exceptions.ConnectionError as e:
+        log.error('msg="Exception raised attempting to connect to Etherpad" exception="%s"' % e)
+        raise AppFailure
+
+
 def savetostorage(wopisrc, acctok, isclose, wopilock):
     '''Copy document from Etherpad back to storage'''
     # get document from Etherpad
     try:
         log.info('msg="Fetching file from Etherpad" isclose="%s" appurl="%s" token="%s"' %
-                 (isclose, appurl + '/p' + _padid(wopilock['docid'][1:]), acctok[-20:]))
+                 (isclose, appurl + '/p' + wopilock['docid'], acctok[-20:]))
         epfile = _fetchfrometherpad(wopilock, acctok)
     except AppFailure:
         return jsonify('Could not save file, failed to fetch document from Etherpad'), http.client.INTERNAL_SERVER_ERROR
@@ -162,7 +166,7 @@ def savetostorage(wopisrc, acctok, isclose, wopilock):
     if wopilock['digest'] != 'dirty':
         # so far the file was not touched: before forcing a put let's validate the contents
         h = hashlib.sha1()
-        h.update(epfile.encode())
+        h.update(epfile)
         if h.hexdigest() == wopilock['digest']:
             log.info('msg="File unchanged, skipping save" token="%s"' % acctok[-20:])
             return '{}', http.client.ACCEPTED
